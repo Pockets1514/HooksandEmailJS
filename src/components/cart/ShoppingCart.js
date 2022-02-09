be64eea1bf9fe5cef37ee6f9ecb2c0dd42f670c5
Steven Paquette <Pockets@stevandcomputer.lan>
2022-02-09 03:10:47 -0700

    added state to ShoppingCart so min/max button will toggle

--- src/components/cart/ShoppingCart.js
+++ src/components/cart/ShoppingCart.js
@@ -1,12 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import CartItem from "./CartItem";
-export default function ShoppingCart({ cart, onAdd }) {
-  const cartStatus = cart.length > 0 ? "cart-visible" : "cart-hidden";
 
+export default function ShoppingCart({ cart, onAdd }) {
+  const [minMaxStatus, updateMinMax] = useState({
+    cartSize: "min",
+  });
+
+  console.log(minMaxStatus);
+
+  const cartVisibility = cart.length > 0 ? "cart-visible" : "cart-hidden";
+  const cartQuantity = cart.length > 0 ? `(${cart.length})` : "";
+  const Minimize = () => {
+    updateMinMax({ cartSize: "min" });
+    // console.log(minMaxStatus);
+  };
+  const Maximize = () => {
+    updateMinMax({ cartSize: "max" });
+    // console.log(minMaxStatus);
+  };
   return (
-    <div className={cartStatus}>
-      <h1 className="cart-header">ShoppingCart</h1>
-      <div className="cart-items">
+    <div className={`${cartVisibility} cart-${minMaxStatus}`}>
+      <div className="cart-header">
+        <h1 className="cart-header__title">ShoppingCart</h1>
+        <h1 className="cart-header__qty">{cartQuantity}</h1>
+        {minMaxStatus.cartSize === "max" ? (
+          <button
+            className="cart-header__make-min cart-header__btn"
+            onClick={Minimize}
+          >
+            __
+          </button>
+        ) : (
+          <button
+            className="cart-header__make-max cart-header__btn"
+            onClick={Maximize}
+          >
+            X
+          </button>
+        )}
+      </div>
+      <div className={`cart-items cart-items-${minMaxStatus}`}>
         {cart.map((item) => (
           <CartItem
             key={item.key}
