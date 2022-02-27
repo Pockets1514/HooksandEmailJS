b8a4f066f8a8d8f15d564c30cdc679ba83bb821b
Steven Paquette <Pockets@stevandcomputer.lan>
2022-02-26 17:59:36 -0700

    updated menu toggle button to rotate 90degrees on click

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -6,17 +6,23 @@ class Header extends Component {
       if (document.getElementById("navbar").classList.contains("hidden")) {
         document.getElementById("navbar").classList.remove("hidden");
         document.getElementById("navbar").classList.add("visible");
+        document.getElementById("toggle-hidden").id = "toggle-visble";
       } else {
         document.getElementById("navbar").classList.remove("visible");
         document.getElementById("navbar").classList.add("hidden");
+        document.getElementById("toggle-visble").id = "toggle-hidden";
       }
     }
 
     return (
       <div className="header">
         <p>The Boho Bakery of Maine</p>
-        <div className="hamburger-wrapper" onClick={MenuToggle}>
-          <i className="fas fa-bars"></i>
+        <div
+          className="hamburger-wrapper"
+          id="toggle-hidden"
+          onClick={MenuToggle}
+        >
+          <i className="fas fa-bars "></i>
         </div>
       </div>
     );
